ca4a62bf5f6906849f3cba5269ffdbf60c7ab699
Gabor Galgoczy <ggabor@gmail.com>
2022-12-01 15:10:00 +0100

    added comments

--- src/main/java/project/models/Camera.java
+++ src/main/java/project/models/Camera.java
@@ -4,6 +4,9 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import project.utils.math.Vector3D;
 
+/**
+ * Represents a 3D camera for rendering
+ */
 public class Camera implements Cloneable {
     public @NotNull Vector3D position = new Vector3D();
     public @NotNull Vector3D rotation = new Vector3D(); // in radians
@@ -12,32 +15,56 @@ public class Camera implements Cloneable {
     private int FOV = 90;
 
 
+    /**
+     * @return field of view
+     */
     public synchronized int getFOV() {
         return FOV;
     }
 
+    /**
+     * @return minimum visible distance from the camera
+     */
     public double getNear() {
         return 0.1;
     }
 
+    /**
+     * @return maximum visible distance from the camera
+     */
     public double getFar() {
         return 200;
     }
 
+    /**
+     * Sets the field of view of the camera
+     * @param FOV new field of view
+     */
     public synchronized void setFOV(int FOV) {
         if (FOV < minFOV) {
             this.FOV = minFOV;
         } else this.FOV = Math.min(FOV, maxFOV);
     }
 
+    /**
+     * Moves the camera by given amount
+     * @param amount amount
+     */
     public synchronized void move(@NotNull Vector3D amount) {
         position = position.add(amount);
     }
 
+    /**
+     * Rotates the camera by given amount
+     * @param amount amount
+     */
     public synchronized void rotate(@NotNull Vector3D amount) {
         rotation = rotation.add(amount);
     }
 
+    /**
+     * @return a clone of the camera
+     */
     @Override
     @Contract(pure = true)
     public @NotNull Camera clone() {
@@ -52,6 +79,10 @@ public class Camera implements Cloneable {
         }
     }
 
+    /**
+     * @param obj another object
+     * @return <code>true</code> - if the two cameras are equal
+     */
     @Override
     public boolean equals(@NotNull Object obj) {
         if (obj == this) {
--- src/main/java/project/models/Room.java
+++ src/main/java/project/models/Room.java
@@ -6,7 +6,13 @@ import project.utils.math.Vector3D;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores information about a room
+ */
 public class Room {
+    /**
+     * Stores information about a wall of the room
+     */
     public static class Wall {
         public @NotNull List<@NotNull Vector3D> corners = new ArrayList<>();
     }
